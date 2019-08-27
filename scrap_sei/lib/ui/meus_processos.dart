607b8b3011ddc9aa88b0ef34ae1dea568078594d
Dennis Verissimo <verissimodennis@gmail.com>
2019-08-27 14:45:55 -0300

    Removendo item com dismissible

--- scrap_sei/lib/ui/meus_processos.dart
+++ scrap_sei/lib/ui/meus_processos.dart
@@ -50,81 +50,109 @@ class _MeusProcessosState extends State<MeusProcessos> {
 
       body: Container(
 
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.all(8.0),
+            children: <Widget>[
 
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.all(8.0),
-          children: <Widget>[
-            ListTile(
-              trailing: Column(
-                children: <Widget>[
-                     Container(
-                      height: 32,
-                      child: Icon(Icons.notifications_active, size: 15, color: Colors.green,),
-                    ),
-                  Text("10/10/10", style: TextStyle(fontSize: 12),),
-                ],
-              ) ,
+              Dismissible(
+                key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+                background: Container(
+                  color: Colors.deepPurpleAccent,
+                ),
+                direction: DismissDirection.startToEnd,
+                child: ListTile(
+                  trailing: Column(
+                    children: <Widget>[
+                         Container(
+                          height: 32,
+                          child: Icon(Icons.notifications_active, size: 15, color: Colors.green,),
+                        ),
+                      Text("10/10/10", style: TextStyle(fontSize: 12),),
+                    ],
+                  ) ,
 
-              title: Text("Viagem"),
-              subtitle: Text("23487.000815/2019-97"),
-              onTap: irParaResumo
-            ),
+                  title: Text("Viagem"),
+                  subtitle: Text("23487.000815/2019-97"),
+                  onTap: irParaResumo
+                ),
+              ),
 
-            Divider(),
+  
 
-            ListTile(
-                trailing: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 32,
-                      child: Icon(Icons.calendar_today, size: 15, color: Colors.grey,),
-                    ),
-                    Text("10/10/10", style: TextStyle(fontSize: 12),),
-                  ],
-                ) ,
+              Dismissible(
+                key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+                background: Container(
+                  color: Colors.deepPurpleAccent,
+                ),
+                direction: DismissDirection.startToEnd,
+                child: ListTile(
+                    trailing: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 32,
+                          child: Icon(Icons.calendar_today, size: 15, color: Colors.grey,),
+                        ),
+                        Text("10/10/10", style: TextStyle(fontSize: 12),),
+                      ],
+                    ) ,
 
-                title: Text("Declaração"),
-                subtitle: Text("23487.000815/2019-97"),
-                onTap: irParaResumo
-            ),
-            Divider(),
+                    title: Text("Declaração"),
+                    subtitle: Text("23487.000815/2019-97"),
+                    onTap: irParaResumo
+                ),
+              ),
 
-            ListTile(
-                trailing: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 32,
-                      child: Icon(Icons.notifications_active, size: 15, color: Colors.green,),
-                    ),
-                    Text("10/10/10", style: TextStyle(fontSize: 12),),
-                  ],
-                ) ,
 
-                title: Text("Atestado médico"),
-                subtitle: Text("23487.000815/2019-97"),
-                onTap: irParaResumo
-            ),
+              Dismissible(
+                key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+                background: Container(
+                  color: Colors.deepPurpleAccent,
+                ),
+                direction: DismissDirection.startToEnd,
+                child: ListTile(
+                    trailing: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 32,
+                          child: Icon(Icons.notifications_active, size: 15, color: Colors.green,),
+                        ),
+                        Text("10/10/10", style: TextStyle(fontSize: 12),),
+                      ],
+                    ) ,
 
-            ListTile(
-                trailing: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 32,
-                      child: Icon(Icons.calendar_today, size: 15, color: Colors.grey,),
-                    ),
-                    Text("10/10/10", style: TextStyle(fontSize: 12),),
-                  ],
-                ) ,
+                    title: Text("Atestado médico"),
+                    subtitle: Text("23487.000815/2019-97"),
+                    onTap: irParaResumo
+                ),
+              ),
 
-                title: Text("Ajuda de custo"),
-                subtitle: Text("23487.000815/2019-97"),
-                onTap: irParaResumo
-            ),
+              Dismissible(
+                key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+                background: Container(
+                  color: Colors.deepPurpleAccent,
+                ),
+                direction: DismissDirection.startToEnd,
+                child: ListTile(
+                    trailing: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 32,
+                          child: Icon(Icons.calendar_today, size: 15, color: Colors.grey,),
+                        ),
+                        Text("10/10/10", style: TextStyle(fontSize: 12),),
+                      ],
+                    ) ,
 
-          ],
+                    title: Text("Ajuda de custo"),
+                    subtitle: Text("23487.000815/2019-97"),
+                    onTap: irParaResumo
+                ),
+              ),
+
+            ],
+          ),
         ),
-      ),
     );
   }
 }
