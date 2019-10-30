611e589fbfea9dafa3e54714adf3bc5ad045c7bd
Dennis Verissimo <verissimodennis@gmail.com>
2019-10-30 14:38:24 -0300

    validando formulario tela processo

--- scrap_sei/lib/ui/novo_processo.dart
+++ scrap_sei/lib/ui/novo_processo.dart
@@ -21,13 +21,11 @@ class _NovoProcessoState extends State<NovoProcesso> {
   String num = "";
   String des = "";
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context){
-
-
     return Scaffold(
-
-
     /*  floatingActionButton: Container(
         padding: EdgeInsets.only(top: 45),
         child: FloatingActionButton(
@@ -62,86 +60,102 @@ class _NovoProcessoState extends State<NovoProcesso> {
       body: SingleChildScrollView(
           padding: EdgeInsets.all(20),
           child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  child: TextFormField(
-                    // autofocus: true,
-                    keyboardType: TextInputType.text,
-                    style: new TextStyle(color: Colors.black45, fontSize: 15),
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(10),
-                        labelText: "Processo",
-                        labelStyle: TextStyle(color: Colors.black, fontSize: 20)
-                    ),
-                    onChanged: (value){
-                      num = value;
-                    },
-                  ),
-                ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: TextFormField(
+                        // autofocus: true,
+                        validator: (value){
+                          if(value.isEmpty) return "o campo é obrigatório.";
+                          if (value.length < 3 )
+                            return "o campo precisa ter mais de 2 caracteres.";
+                          return null;
+                        },
 
-
-                Container(
-                  padding: EdgeInsets.only(top: 20),
-                  child: TextFormField(
-                    // autofocus: true,
-                    keyboardType: TextInputType.text,
-                    style: new TextStyle(color: Colors.black45, fontSize: 15),
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(10),
-                        labelText: "Descrição",
-                        labelStyle: TextStyle(color: Colors.black, fontSize: 20, )
-                    ),
-                    onChanged: (value){
-                      des = value;
-                    },
-                  ),
-                ),
-
-
-                Container(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                          child: Image.asset("imagens/infra.png",
-                              fit: BoxFit.fitWidth ,
-                            ),
-                        margin: EdgeInsets.only(right: 10),
-                      ),
-                      Expanded(child: TextFormField(
                         keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          hintText: "Código da imagem",
-                        ),
-
                         style: new TextStyle(color: Colors.black45, fontSize: 15),
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.all(10),
+                            labelText: "Processo",
+                            labelStyle: TextStyle(color: Colors.black, fontSize: 20)
+                        ),
+                        onChanged: (value){
+                          num = value;
+                        },
                       ),
-                      )
-                    ],
-                  ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.only(top: 20),
+                      child: TextFormField(
+                        // autofocus: true,
+                        validator: (value){
+                          if(value.isEmpty) return "o campo é obrigatório.";
+                          if (value.length < 3 )
+                            return "o campo precisa ter mais de 2 caracteres.";
+                          return null;
+                        },
+                        keyboardType: TextInputType.text,
+                        style: new TextStyle(color: Colors.black45, fontSize: 15),
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.all(10),
+                            labelText: "Descrição",
+                            labelStyle: TextStyle(color: Colors.black, fontSize: 20, )
+                        ),
+                        onChanged: (value){
+                          des = value;
+                        },
+                      ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                              child: Image.asset("imagens/infra.png",
+                                  fit: BoxFit.fitWidth ,
+                                ),
+                            margin: EdgeInsets.only(right: 10),
+                          ),
+                          Expanded(child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              hintText: "Código da imagem",
+                            ),
+
+                            style: new TextStyle(color: Colors.black45, fontSize: 15),
+                          ),
+                          )
+                        ],
+                      ),
+                    ),
+
+
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: RaisedButton(
+                        onPressed: () async{
+                          if(_formKey.currentState.validate()){
+                            await MyDatabase.instance.addProcesso(Processo(numero: num, nome: des,));
+                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (contexto) => MeusProcessos() ));
+                          }
+                        },
+                        child:
+                        Text("ADICIONAR",
+                          style: TextStyle(color: Colors.white),),
+                        color: Colors.deepPurpleAccent,
+
+                      ),
+                    )
+
+                  ],
                 ),
-
-
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: RaisedButton(
-                    onPressed: () async{
-                      await MyDatabase.instance.addProcesso(Processo(numero: num, nome: des,));
-                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (contexto) => MeusProcessos() ));
-                    },
-                    child:
-                    Text("ADICIONAR",
-                      style: TextStyle(color: Colors.white),),
-                    color: Colors.deepPurpleAccent,
-
-                  ),
-                )
-
-              ],
-            ),
+              ),
           ),
         ),
     );
@@ -149,3 +163,8 @@ class _NovoProcessoState extends State<NovoProcesso> {
 
 
 }
+
+/*
+Scaffold.of(context).showSnackBar(SnackBar(content: Text("Adicionando processo"),
+                           duration: Duration( seconds: 3),));
+ */
